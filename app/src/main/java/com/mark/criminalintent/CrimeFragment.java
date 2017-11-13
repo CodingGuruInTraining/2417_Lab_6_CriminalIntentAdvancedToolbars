a1bf8cbd88c336bc9f6bbcd55298771135e89731
MF <notsure@compuserv.com>
2017-11-13 15:29:17 -0600

    lab 6 advanced - toolbars and menus

--- app/src/main/java/com/mark/criminalintent/CrimeFragment.java
+++ app/src/main/java/com/mark/criminalintent/CrimeFragment.java
@@ -1,13 +1,17 @@
 package com.mark.criminalintent;
 
+import android.annotation.TargetApi;
 import android.app.Activity;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
+import android.support.v4.app.NavUtils;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
@@ -45,15 +49,24 @@ public class CrimeFragment extends Fragment {
         UUID crimeId = (UUID)getArguments().getSerializable(EXTRA_CRIME_ID);
         // Retrieves specific Crime.
         mCrime = CrimeLab.get(getActivity()).getCrime(crimeId);
+
+        setHasOptionsMenu(true);
     }
 
     private void updateDate() {
         mDateButton.setText(mCrime.getDate().toString());
     }
 
+    @TargetApi(11)
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup parent, Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_crime, parent, false);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
+            if (NavUtils.getParentActivityName(getActivity()) != null) {
+                getActivity().getActionBar().setDisplayHomeAsUpEnabled(true);
+            }
+        }
 
         // Populates widgets.
         mTitleField = (EditText)v.findViewById(R.id.crime_title);
@@ -122,6 +135,19 @@ public class CrimeFragment extends Fragment {
             mCrime.setDate(date);
 //            mDateButton.setText(mCrime.getDate().toString());
             updateDate();
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                if (NavUtils.getParentActivityName(getActivity()) != null) {
+                    NavUtils.navigateUpFromSameTask(getActivity());
+                }
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
         }
     }
 }
--- app/src/main/java/com/mark/criminalintent/CrimeLab.java
+++ app/src/main/java/com/mark/criminalintent/CrimeLab.java
@@ -21,13 +21,13 @@ public class CrimeLab {
         mAppContext = appContext;
         mCrimes = new ArrayList<Crime>();
 
-        // Generates list of fake crimes.
-        for (int i = 0; i < 100; i++) {
-            Crime c = new Crime();
-            c.setTitle("Crime #" + i);
-            c.setSolved(i % 2 == 0); // Every other crime is solved.
-            mCrimes.add(c);
-        }
+//        // Generates list of fake crimes.
+//        for (int i = 0; i < 100; i++) {
+//            Crime c = new Crime();
+//            c.setTitle("Crime #" + i);
+//            c.setSolved(i % 2 == 0); // Every other crime is solved.
+//            mCrimes.add(c);
+//        }
     }
     // Custom getter.
     public static CrimeLab get(Context c) {
@@ -36,6 +36,12 @@ public class CrimeLab {
         }
         return sCrimeLab;
     }
+
+
+    public void addCrime(Crime c) {
+        mCrimes.add(c);
+    }
+
 
     public ArrayList<Crime> getCrimes() {
         return mCrimes;
